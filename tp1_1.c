d65fc307b016b89f63aa3e90944bbbddd14798d1
Gonzalo Javier Martinez <goncham94@gmail.com>
2025-03-25 13:23:11 -0300

    elimino .exe

--- tp1_1.c
+++ tp1_1.c
@@ -10,6 +10,6 @@ printf ("\n Contenido del Puntero: %d", *pvariable);
 printf ("\n Dirección de memoria almacenada en el puntero: %d", pvariable);
 printf ("\n Dirección de memoria de la variable: %d", &variable);
 printf ("\n Dirección de memoria del puntero: %d", &pvariable);
-
+printf ("\n Espacio ocupado por la variable: %d bytes", sizeof(variable));
 return 0;
 }
